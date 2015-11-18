675c0a234ab7070c2e9713447fa539da3f1a362b
BrandonQi <bqi@stevens.edu>
2015-11-17 23:21:44 -0500

    add fresh option

--- SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivity.java
+++ SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivity.java
@@ -12,29 +12,4 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_settings_1:
-                break;
-            case R.id.action_settings_2:
-                break;
-            case R.id.action_settings_3:
-                break;
-            case R.id.action_settings_4:
-                break;
-            default:
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
