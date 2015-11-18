d78d9ea98690aaa4837d1693a0e6f31e9ab144c8
BrandonQi <bqi@stevens.edu>
2015-11-17 23:00:43 -0500

    created getWeatherTask.class

--- SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivity.java
+++ SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+    public final String LOG_CAT = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +22,17 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings_1:
+                break;
+            case R.id.action_settings_2:
+                break;
+            case R.id.action_settings_3:
+                break;
+            case R.id.action_settings_4:
+                break;
+            default:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
--- SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivityFragment.java
+++ SunShine/app/src/main/java/com/example/binqi/sunshine/MainActivityFragment.java
@@ -1,13 +1,24 @@
 package com.example.binqi.sunshine;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,11 +26,30 @@ import java.util.Arrays;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    public final String LOG_CAT = MainActivityFragment.class.getSimpleName();
 
     ArrayAdapter<String> mForecastAdapter;
+
     public MainActivityFragment() {
     }
-
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater menuInflater){
+        menuInflater.inflate(R.menu.menu_mainfragment, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id = menuItem.getItemId();
+        if(id == R.id.refresh){
+            new getWeatherTask().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,4 +73,37 @@ public class MainActivityFragment extends Fragment {
         listView.setAdapter(mForecastAdapter);
         return rootView;
     }
+
+    public class getWeatherTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                downloadURL("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=3cd1fb7bb429a24860f198184c4332b9");
+                return null;
+            }catch (IOException e) {
+                Log.e(LOG_CAT, "url failed!");
+                return null;
+            }
+        }
+        public Void downloadURL(String urlString) throws IOException {
+            URL url = new URL(urlString);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+            int respenseCode = httpURLConnection.getResponseCode();
+            Log.d(LOG_CAT,"response is" + respenseCode);
+            String weather = readIT(httpURLConnection.getInputStream());
+            return null;
+        }
+        public String readIT(InputStream it) throws IOException {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(it));
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        }
+    }
+
 }
